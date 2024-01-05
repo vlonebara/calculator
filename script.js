40e4ccba8d9c95112432134cc61c957770cecc14
Kirill Fedorov <vlonebara@yandex.ru>
2024-01-05 17:31:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.querySelector(".display");
+let btns = Array.from(document.querySelectorAll(".button"));
+
+btns.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "0";
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+        break;
+      default:
+        if (display.innerText === "0") {
+          display.innerText = e.target.innerText;
+        } else {
+          display.innerText += e.target.innerText;
+        }
+    }
+  });
+});
