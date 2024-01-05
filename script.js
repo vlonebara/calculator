96e01a99b2f35045246000a284dc0d34d3d2fa37
Kirill Fedorov <vlonebara@yandex.ru>
2024-01-05 17:47:05 +0300

    added max length

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ btns.map((button) => {
         display.innerText = eval(display.innerText);
         break;
       default:
-        if (display.innerText === "0") {
-          display.innerText = e.target.innerText;
-        } else {
-          display.innerText += e.target.innerText;
+        if (display.innerText.length < 10) {
+          if (display.innerText === "0") {
+            display.innerText = e.target.innerText;
+          } else {
+            display.innerText += e.target.innerText;
+          }
         }
     }
   });
