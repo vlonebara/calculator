2e9971963ecec3c05e8a97663adaf24f436c783f
Kirill Fedorov <vlonebara@yandex.ru>
2024-01-06 19:44:57 +0300

    added sounds for btns, fixes and etc

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 let display = document.querySelector(".display");
 let btns = Array.from(document.querySelectorAll(".button"));
+const btnAudio = new Audio("audio/btn.wav");
+const equalAudio = new Audio("audio/equal.wav");
+let operation = "";
+let a = "";
+let b = "";
 
 btns.map((button) => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
       case "AC":
+        btnAudio.play();
         display.innerText = "0";
+        operation = "";
+        a = "";
+        b = "";
         break;
       case "=":
-        display.innerText = eval(display.innerText);
+        equalAudio.play();
+        display.innerText = eval(a + operation + b);
+        operation = "";
+        a = display.innerText;
+        b = "";
+        break;
+      case "+":
+        btnAudio.play();
+        operation = "+";
+        break;
+      case "-":
+        btnAudio.play();
+        operation = "-";
         break;
       default:
-        if (display.innerText.length < 10) {
+        if (display.innerText.length < 9) {
           if (display.innerText === "0") {
+            btnAudio.play();
             display.innerText = e.target.innerText;
+            a += e.target.innerText;
           } else {
-            display.innerText += e.target.innerText;
+            btnAudio.play();
+            if (operation === "") {
+              display.innerText += e.target.innerText;
+              a += e.target.innerText;
+              break;
+            } else {
+              if (b === "") {
+                display.innerText = e.target.innerText;
+              } else {
+                display.innerText += e.target.innerText;
+              }
+              b += e.target.innerText;
+              break;
+            }
           }
         }
     }
   });
+});
+
+display.addEventListener("click", (e) => {
+  if (display.innerText.length > 1) {
+    display.innerText = display.innerText.slice(0, -1);
+  }
 });
